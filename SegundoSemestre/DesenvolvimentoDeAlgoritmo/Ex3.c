95a81b9cfd36a5070b39e52e9f84ff161a04b171
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2024-08-26 15:42:23 -0300

    feat: Exercício Incompleto

---
+++ SegundoSemestre/DesenvolvimentoDeAlgoritmo/Ex3.c
@@ -0,0 +1,20 @@
+/*Cadastro de Produtos usando Struct: Crie uma estrutura de dados chamada "Produto" que armazena nome, preço e quantidade em estoque. 
+Permita ao usuário cadastrar produtos, exibir as informações e calcular o valor total em estoque (preço * quantidade).*/
+
+#include <stdio.h>
+
+typedef struct{
+
+    char nome[30];
+    float preco;
+    int qttEstoque;
+
+}Produto;
+
+
+int main(void){
+
+    
+
+    return 0;
+}
