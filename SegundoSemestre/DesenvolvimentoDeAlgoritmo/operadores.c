46cd66f5bd184886ba5a32637bf031a2c4fc1bc2
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2024-08-26 18:38:08 -0300

    feat: Aula 2 - Operadores

---
+++ SegundoSemestre/DesenvolvimentoDeAlgoritmo/operadores.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+//Exemplo de recursão
+
+// int fatorial(int num)
+// {
+//     if (num == 0)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return num * fatorial(num - 1);
+//     }
+// }
+
+// int main(void)
+// {
+
+//     int num;
+//     scanf("%d", &num);
+//     printf("%d! = %d", num, fatorial(num));
+
+//     return 0;
+// }
+
+
+//Exemplo Vetores
+
+// int main(void){
+
+//     int vetor [5];
+
+//     for(int i = 0; i < 5; i++)
+//     {
+//         scanf("%d", &vetor[i]);
+//     }
+
+//     for(int i = 0; i < 5; i++)
+//     {
+//         printf("%d", vetor[i]);
+//     }
+
+
+//     return 0;
+// }
