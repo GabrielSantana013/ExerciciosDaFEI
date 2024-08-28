a2514d315220208e489e8e41dbd84c7a16727112
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-08-27 21:06:01 -0300

    feat: exercício concluído

--- SegundoSemestre/DesenvolvimentoDeAlgoritmo/Exercicios_para_nota/Ex3.c
+++ SegundoSemestre/DesenvolvimentoDeAlgoritmo/Exercicios_para_nota/Ex3.c
@@ -2,7 +2,8 @@
 Permita ao usuário cadastrar produtos, exibir as informações e calcular o valor total em estoque (preço * quantidade).*/
 
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct Produto
 {
@@ -12,34 +13,75 @@ typedef struct Produto
 
 } Produto;
 
-void cadastrar(struct Produto *ptr)
-{
-    //limpa o buffer
-    setbuf(stdin, NULL);
-    printf("Digite o nome do produto: ");
-    gets(ptr->nome);
-    printf("Digite o preco do produto: ");
-    scanf("%f", &ptr->preco);
-    printf("Digite a quantidade do produto: ");
-    scanf("%d", &ptr->qttEstoque);
 
+int cadastrar(struct Produto *ptr)
+{
+    int n;
+    printf("Digite a quantidade de produtos a serem cadastrados: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        // limpa o buffer
+        setbuf(stdin, NULL);
+        printf("Digite o nome do produto: ");
+        fgets(ptr->nome, sizeof(ptr->nome), stdin);
+        printf("Digite o preco do produto: ");
+        scanf("%f", &ptr->preco);
+        printf("Digite a quantidade do produto: ");
+        scanf("%d", &ptr->qttEstoque);
+        ptr++;
+    }
+    return n;
 }
 
 void exibirItens(struct Produto *ptr){
 
-    char buscaNome[30];
-    printf("Digite o nome do produto para ver seus dados: ");
+    char nome[30];
+    int achou = 1;
+    printf("\nDigite o nome do produto para ver seus dados: ");
+    //limpa buffer
+    setbuf(stdin, NULL);
+    fgets(nome, sizeof(nome), stdin);
 
+    for(int i = 0; i < 20; i++)
+    {
+        if(strcmp(nome, ptr->nome) == 0)
+        {
+            printf("\nNome: %s", ptr->nome);
+            printf("Preco: %.2f\n", ptr->preco);
+            printf("Quantidade em estoque: %d\n\n", ptr->qttEstoque);
+            achou--;
+        }
+        ptr++;
+    }
+    if(achou)
+    {
+        printf("\nProduto nao encontrado\n\n");
+    }
+}
+
+void calcularValorTotal(struct Produto *ptr, int produtosCadastrados)
+{
+    float total = 0.0;
+    for (int i = 0; i < produtosCadastrados; i++)
+    {
+        total += ptr->preco * ptr->qttEstoque;
+        ptr++;
+    }
+    printf("Valor total em estoque: %.2f\n", total);
 }
 
 int main(void)
 {
 
-    Produto *ptr, produtos;
-    ptr = &produtos;
+    Produto *ptr, produtos[20];
+    ptr = &produtos[0];
     int menu;
+    int produtosCadastrados = 0;
     do
     {
+        printf("\tMaximo 20 itens\n");
         printf("\t1 - Cadastrar item\n");
         printf("\t2 - Exibir itens\n");
         printf("\t3 - Calcular valor total\n");
@@ -49,10 +91,13 @@ int main(void)
         switch (menu)
         {
         case 1:
-            cadastrar(ptr);
+            produtosCadastrados+= cadastrar(ptr);
             break;
         case 2:
             exibirItens(ptr);
+            break;
+        case 3:
+            calcularValorTotal(ptr, produtosCadastrados);
             break;
         case 0:
             exit(0);
