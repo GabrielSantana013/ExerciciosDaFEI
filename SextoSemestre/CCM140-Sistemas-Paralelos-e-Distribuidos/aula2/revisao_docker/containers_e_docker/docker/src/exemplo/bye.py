5863e33cc74298312b3ccbdc76fb8f531a992ef1
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2026-08-19 23:29:38 +0000

    feat: exemplos da revisao de docker aula 2

---
+++ SextoSemestre/CCM140-Sistemas-Paralelos-e-Distribuidos/aula2/revisao_docker/containers_e_docker/docker/src/exemplo/bye.py
@@ -0,0 +1,5 @@
+from time import sleep
+
+while True:
+    print("PEDRAO filhodaputa", flush=True)
+    sleep(1)
---
+++ SextoSemestre/CCM140-Sistemas-Paralelos-e-Distribuidos/aula2/revisao_docker/containers_e_docker/docker/src/exemplo/hello.py
@@ -0,0 +1,5 @@
+from time import sleep
+
+while True:
+    print("uuuuuu uuuu uuuu", flush=True)
+    sleep(1)
